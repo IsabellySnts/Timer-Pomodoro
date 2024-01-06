8b91f97d947af4ea386aff3f0440ad66b9304245
Isabelly <isabellyd429@gmail.com>
2024-01-06 16:46:50 -0300

    :lipstick: Definindo estilos globais - fontes e cores

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { Button } from "./components/Button/Button";
 import { ThemeProvider } from "styled-components";
 import { DefaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
   return (
-      <ThemeProvider theme={DefaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="danger" />
-        <Button variant="success" />
-        <Button />
-      </ThemeProvider>
-    
+    <ThemeProvider theme={DefaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+
   )
 }
 
