b530fcbdbba6995dde54e0990ed686f06db9d49c
Isabelly <isabellyd429@gmail.com>
2024-01-06 18:13:00 -0300

    :sparkles::bricks::lipstick: Criando rotas das telas e Layout padrão de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Button } from "./components/Button/Button";
 import { ThemeProvider } from "styled-components";
 import { DefaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Route";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
 
   return (
     <ThemeProvider theme={DefaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
 
   )
